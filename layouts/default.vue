3372f7d4d4ce96bbaa8c754391620a52dbe77430
zhaogongchengsi <zzh1586169624@163.com>
2023-09-27 08:41:28 +0800

    chore: 删除多余的代码

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,7 +18,6 @@ const app = useAppConfig()
 	<footer class="border-t border-color-gray-200">
 		<div class="container mx-auto flex justify-between items-center px-3 py-3">
 			<h3>{{ app.title }}</h3>
-			<!-- <AppDarkToggle /> -->
 		</div>
 	</footer>
 </template>
