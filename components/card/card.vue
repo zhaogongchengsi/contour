1d0af2bd53158a51632ccc570a162a89f13b6fbc
zhaogongchengsi <zzh1586169624@163.com>
2023-10-16 17:21:26 +0800

    chore: 删除多于代码

--- components/card/card.vue
+++ components/card/card.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { computed, provide } from 'vue';
 import { options, key } from './props'
 import cardIcon from './card-icon.vue'
 import cardInner from './card-inner.vue'
@@ -61,72 +60,3 @@ provide(key, props)
 		</card-inner>
 	</ui-card-size>
 </template>
-
-<style>
-.card-inner_1x1 {
-	grid-template-rows: 1fr;
-	grid-template-columns: 1fr;
-	grid-template-areas: 'icon';
-}
-
-.card-inner_2x2 {
-	width: 100%;
-	height: 100%;
-	grid-template-rows: repeat(4, 1fr);
-	grid-template-columns: repeat(4, 1fr);
-
-	grid-template-areas:
-		'image image image icon'
-		"image image image ."
-		"image image image ."
-		"name name name name";
-}
-
-.card-inner_icon {
-	grid-area: icon;
-}
-
-.card-inner_image,
-.card-inner_image_upload {
-	grid-area: image;
-}
-
-.card-inner_name {
-	grid-area: name;
-}
-
-.card-icon_1x1 {
-	width: 100%;
-	height: 100%;
-}
-
-.card-icon_2x2 {
-	width: var(--card-size);
-	height: var(--card-size);
-}
-
-.card-icon_1x2 {
-	width: calc(var(--card-size) - calc(var(--card-gap-x) * 2));
-	height: var(--card-size);
-}
-
-.card-icon_2x1 {
-	height: calc(var(--card-size) - calc(var(--card-gap-x) * 2));
-	width: var(--card-size);
-}
-
-.card-icon_2x4 {
-	width: 100%;
-	height: calc(var(--card-size) * 2);
-}
-
-.card-icon_4x2 {
-	height: calc(calc(var(--card-size) * 2) - calc(var(--card-gap-x) * 2));
-	width: calc(var(--card-size) * 2);
-}
-
-.card-icon_4x4 {
-	width: calc(var(--card-size) * 2);
-	height: calc(var(--card-size) * 2);
-}
-</style>
