e142430d4902dadba0395af44e56b6e58202fe11
zhaogongchengsi <zzh1586169624@163.com>
2023-10-18 15:29:48 +0800

    chore: 啥也没干

--- components/ui/image-selector.vue
+++ components/ui/image-selector.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
+import { useLoadingBar } from 'naive-ui'
 
-
-const images = ['github.webp', 'grid_01.webp', 'grid_10.webp', 'grid.webp', 'img2.webp'].map(i => `/images/${i}`)
+const images = ref(['github.webp', 'grid_01.webp', 'grid_10.webp', 'grid.webp', 'img2.webp'].map(i => `/images/${i}`))
 
 const props = withDefaults(defineProps<{ value: string, prefix?: string, separator?: string }>(), {
 	prefix: '',
 	separator: ':'
 })
 
+const m = 1024 * 1024
+const loadingBar = useLoadingBar()
 const emit = defineEmits(["update:value", 'change'])
 const data = useVModel(props, 'value', emit)
+const { files, open, reset, onChange } = useFileDialog({
+	accept: 'image/*',
+	multiple: false
+})
 
 const format = (value: string): string => {
 	return [props.prefix, value].filter(Boolean).join(props.separator)
@@ -19,12 +25,19 @@ const itemClick = (value: string) => {
 	data.value = format(value)
 }
 
+onChange(async (files) => {
+	const file = files![0]
+	console.log(file.size / m);
+	
+	const url = URL.createObjectURL(file)
+	images.value.push(url)
+})
 
 </script>
 
 <template>
 	<div class="flex flex-col gap-3 py-2">
-		<div class="flex gap-2 justify-center items-center rounded-xl bg-gray-200 py-1 cursor-pointer">
+		<div class="flex gap-2 justify-center items-center rounded-xl bg-gray-200 py-1 cursor-pointer" @click="open()">
 			<div class="md-icon i-carbon:cloud-upload" />
 			<span>上传</span>
 		</div>
--- components/ui/picture-selector.vue
+++ components/ui/picture-selector.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { NPopover, NTabs, NTabPane, NScrollbar } from 'naive-ui'
+import { NPopover, NTabs, NTabPane, NScrollbar, NLoadingBarProvider } from 'naive-ui'
 
+const loadingBarTargetRef = ref<undefined | HTMLElement>(undefined)
 const value = ref('')
 const props = withDefaults(defineProps<{ value?: string; separator?: string }>(), {
 	separator: ':'
@@ -9,7 +10,7 @@ const props = withDefaults(defineProps<{ value?: string; separator?: string }>()
 const emit = defineEmits(["update:value", 'change'])
 const data = useVModel(props, 'value', emit)
 
-watch(value, (newValue) => {	
+watch(value, (newValue) => {
 	data.value = newValue
 	emit('change', newValue)
 })
@@ -24,19 +25,21 @@ provide('ui-picture-selector-separator', props.separator)
 		<template #trigger>
 			<slot />
 		</template>
-		<div class="w-110">
-			<n-tabs type="bar" animated placement="bottom" size="small">
-				<n-tab-pane name="image" tab="图片">
-					<n-scrollbar class="h-80">
-						<ui-image-selector prefix="url" v-model:value="value" />
-					</n-scrollbar>
-				</n-tab-pane>
-				<n-tab-pane name="emoji" tab="表情">
-					<n-scrollbar class="h-80">
-						<ui-emoji-selector prefix="emoji" v-model:value="value" />
-					</n-scrollbar>
-				</n-tab-pane>
-			</n-tabs>
-		</div>
+		<n-loading-bar-provider :to="loadingBarTargetRef" container-style="position: absolute;">
+			<div class="w-110" ref="loadingBarTargetRef">
+				<n-tabs type="bar" animated placement="bottom" size="small" >
+					<n-tab-pane name="image" tab="图片" display-directive="show">
+						<n-scrollbar class="h-80">
+							<ui-image-selector prefix="url" v-model:value="value" />
+						</n-scrollbar>
+					</n-tab-pane>
+					<n-tab-pane name="emoji" tab="表情" display-directive="show">
+						<n-scrollbar class="h-80">
+							<ui-emoji-selector prefix="emoji" v-model:value="value" />
+						</n-scrollbar>
+					</n-tab-pane>
+				</n-tabs>
+			</div>
+		</n-loading-bar-provider>
 	</n-popover>
 </template>
