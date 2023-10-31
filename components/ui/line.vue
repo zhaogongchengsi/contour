132603b77b107877cf8744136682595d85222b08
zhaogongchengsi <zzh1586169624@163.com>
2023-10-31 09:11:43 +0800

    chore: 初始化数据表格

--- components/ui/line.vue
+++ components/ui/line.vue
@@ -1,15 +1,47 @@
 <script setup lang="ts">
+import { Chart } from "@antv/g2";
+import type { Chart as IChart } from "@antv/g2";
+import { Renderer } from "@antv/g-webgl";
+import theme from "~/assets/g2-theme.json";
 
 const lineDom = ref<HTMLDivElement | null>()
 
-const chart = useChart()
+const chart = shallowRef<IChart>()
 
-onMounted(() => {
-	chart.render(lineDom.value!)
+const init = async () => {
+	const { width, height } = lineDom.value!.getBoundingClientRect()
+
+	chart.value = new Chart({
+		width,
+		height,
+		autoFit: true,
+		container: lineDom.value!,
+		renderer: new Renderer(),
+		margin: 10,
+	});
+
+	chart.value.theme(theme);
+
+}
+
+const load = async () => {
+	chart.value!
+		.line()
+		.data({
+			type: 'fetch',
+			value:
+				'/aapl.csv',
+		})
+		.encode('x', 'date')
+		.encode('y', 'close');
+
+	chart.value!.render()
+}
+
+onMounted(async () => {
+	await init()
+	await load()
 })
-
-console.log(chart)
-
 
 </script>
 
